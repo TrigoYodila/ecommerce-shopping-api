1971046247dc580ed626f61bfde1d490693ae3e7
Trigo YODILA <trigoyodila1996@gmail.com>
2023-02-06 12:20:04 +0100

    Feat:create authrootes login and register

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,26 @@
-const router = require("express").Router()
+const { verifyTokenAndAutorization } = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const User = require("../models/User");
 
-router.get("/usertest", (req, res)=>{
-    res.send("user test successfull")
-})
+const router = require("express").Router();
 
-router.post("/userposttest", (req, res)=>{
-    const username = req.body.username
-    console.log(username)
-})
+//UPDATE
+router.put("/:id", verifyTokenAndAutorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
 
-module.exports = router
+  try{
+    const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body
+    },{new:true})
+    res.status(200).json(updatedUser);
+  }catch(err){
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
